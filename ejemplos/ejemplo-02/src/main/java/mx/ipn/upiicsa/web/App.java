61d3932b120b7e1a26243a25e4e7bf0ddfb35de8
Hermes Francisco Montes Casiano <hermes.montes@gmail.com>
2025-11-07 14:41:16 -0600

    feat: se versionan los proyectos de ejemplos

---
+++ ejemplos/ejemplo-02/src/main/java/mx/ipn/upiicsa/web/App.java
@@ -0,0 +1,10 @@
+package mx.ipn.upiicsa.web;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ejemplos/ejemplo-03/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<body>
+<h2><%= "Hello World!" %></h2>
+<ul>
+<%
+    Integer i=0;
+for(i=0;i<10;i++) {
+%>
+<li>El número de la iteracion es <%=i%></li>
+<%}%>
+</ul>
+</body>
+</html>
