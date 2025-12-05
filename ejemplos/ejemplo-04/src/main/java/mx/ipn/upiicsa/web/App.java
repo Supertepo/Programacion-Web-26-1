f565d86b6c41572e72db2e422cecb5296d09b7b1
Hermes Francisco Montes Casiano <hermes.montes@gmail.com>
2025-12-05 00:55:04 -0600

    feat: ejemplo de formulario de registro

--- ejemplos/ejemplo-04/src/main/java/mx/ipn/upiicsa/web/App.java
+++ ejemplos/ejemplo-04/src/main/java/mx/ipn/upiicsa/web/App.java
@@ -2,7 +2,6 @@ package mx.ipn.upiicsa.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class App {
